5ac6ade3a6dfbfa8bac083ae4ee6b9a57b52ca30
Nandini Jain <nandini.nj26@gmail.com>
2024-06-16 17:07:34 +0530

    added sign up class and connectivity established

--- src/FinanceManager/Conn.java
+++ src/FinanceManager/Conn.java
@@ -1,5 +1,20 @@
 
 package FinanceManager;
-
 import java.sql.*;
+
+public class Conn{
+    Connection connection;
+
+    public Conn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ExpenseManage","root","data@java")
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
 
